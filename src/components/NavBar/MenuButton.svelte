a9bdd4036032dd71ff1c38a60e5a36bda7269129
kirisauce <kirisauce@163.com>
2026-03-16 00:01:30 +0800

    feat: NavBar update. Add widgets to navigation bar.

---
+++ src/components/NavBar/MenuButton.svelte
@@ -0,0 +1,48 @@
+<script module lang="ts">
+  export const sym = Symbol('MenuButton.svelte');
+</script>
+
+<script lang="ts">
+  import { toggle } from '../../client/toggler';
+
+  const {
+    target,
+    children,
+  }: {
+    target: string;
+    children?: any;
+  } = $props();
+
+  function onclick(event: Event) {
+    const el = document.querySelector(target) as HTMLElement;
+    if (!el) return;
+    toggle(el);
+  }
+</script>
+
+<button class="square-button" {onclick}>
+  {@render children()}
+</button>
+
+<style lang="stylus">
+  button {
+    display: flex;
+    color: inherit;
+    flex-direction: row;
+    align-items: center;
+    padding: 6px;
+    border: none;
+    border-radius: 4px;
+    background-color: transparent;
+    transition: background-color var(--expressive-default-effects);
+    border-radius: 6px;
+
+    &:hover {
+      background-color: var(--primary-container);
+    }
+
+    &:active {
+      background-color: var(--secondary-container);
+    }
+  }
+</style>
