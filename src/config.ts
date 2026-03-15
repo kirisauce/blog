1b99aa81d2f712f8298b7702aaa45ee6d9c9455e
kirisauce <kirisauce@163.com>
2026-03-15 16:14:28 +0800

    feat: NavBar

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
-import { defineSiteConfig } from './types/config';
+import { defineNavBarConfig, defineSiteConfig } from './types/config';
 
 export const site = defineSiteConfig({
   title: "kirisauce's blog",
   titleTemplate: "kirisauce's blog",
+});
+
+export const navbar = defineNavBarConfig({
+  icon: 'mdi:home-variant-outline',
+  homeText: 'Home',
 });
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,5 +1,7 @@
 import { z } from 'astro/zod';
 
+// ----- Site Config -----
+
 export const zSiteConfig = z.object({
   title: z.string(),
   titleTemplate: z.string().optional(),
@@ -7,10 +9,23 @@ export const zSiteConfig = z.object({
 
   coverImageStyle: z
     .enum(['static', 'parallax', 'half-parallax'])
-    .default('half-parallax'),
+    .default('parallax'),
 });
 
 export type SiteConfig = z.infer<typeof zSiteConfig>;
 export type SiteConfigInput = z.input<typeof zSiteConfig>;
 export const defineSiteConfig = (config: SiteConfigInput): SiteConfig =>
   zSiteConfig.parse(config);
+
+// ----- NavBar Config -----
+
+export const zNavBarConfig = z.object({
+  icon: z.string().optional(),
+  homeText: z.string().optional(),
+});
+
+export type NavBarConfig = z.infer<typeof zNavBarConfig>;
+export type NavBarConfigInput = z.input<typeof zNavBarConfig>;
+
+export const defineNavBarConfig = (config: NavBarConfigInput): NavBarConfig =>
+  zNavBarConfig.parse(config);
